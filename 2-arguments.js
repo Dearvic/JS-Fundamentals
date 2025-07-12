76b63175e9728f3db559e3ccb398146e4e60e64e
Victor Azode <victor.azode@gmail.com>
2025-07-12 14:39:07 +0100

    Create and update file 2-arg...

---
+++ 2-arguments.js
@@ -0,0 +1,10 @@
+const args = process.argv;
+const sizeOfArguments = args.length;
+
+if (sizeOfArguments <= 2) {
+  console.log("No argument");
+} else if (sizeOfArguments === 3) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
