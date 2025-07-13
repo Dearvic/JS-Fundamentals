b66cc6df73773b578b6ffced846f7211e6685038
Victor Azode <victor.azode@gmail.com>
2025-07-13 10:51:31 +0100

    Checkout the 1-multi..file

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -3,4 +3,3 @@ console.log("Python is cool");
 console.log("JavaScript is amazing");
 // This code prints three statements to the console, demonstrating basic output in JavaScript.
 // It is useful for learning how to display messages in a JavaScript environment, such as Node.js or a web browser console.
-console.log("C is fun");
