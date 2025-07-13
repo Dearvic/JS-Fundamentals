9cc3193211078a740acbdab33d90aeac9becc9e1
Victor Azode <victor.azode@gmail.com>
2025-07-13 09:33:26 +0100

    Create seventh file, 6-multi...

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+const languages = [
+  "C is fun",
+  "Python is cool",
+  "JavaScript is amazing"
+];
+
+console.log(languages.join("\n"));
+// This code prints multiple strings from an array, each on a new line.
+// It is useful for displaying a list of items in a readable format, especially in command line applications.
